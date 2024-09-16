82bbd60985581358049004b1b6fcb8174d2f5737
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-09-16 13:22:04 +0300

    ignore eslint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,15 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/old-api/:path*', // Proxy all requests starting with /api
-        destination: 'http://localhost:8000/api/:path*', // Proxy to backend at localhost:8000
+        source: "/old-api/:path*", // Proxy all requests starting with /api
+        destination: "http://localhost:8000/api/:path*", // Proxy to backend at localhost:8000
       },
     ];
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
