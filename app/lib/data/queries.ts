93eee4487cb14338eeab88b99bfb917bb34717f9
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-09-18 19:33:19 +0300

    Get table name prefixing for tension climbs to work

--- app/lib/data/queries.ts
+++ app/lib/data/queries.ts
@@ -2,6 +2,15 @@ import { sql } from "@vercel/postgres";
 import { BoardRouteParametersWithUuid, BoardRouteParameters, BoulderProblem, ParsedBoardRouteParametersWithUuid, ParsedBoardRouteParameters, SearchRequest, SearchRequestPagination } from "../types";
 import { PAGE_LIMIT } from "@/app/components/board-page/constants";
 
+const getTableName = (board_name: string, table_name: string) => {
+  switch (board_name) {
+    case "tension":
+      return `tension_${table_name}`;
+    default:
+      return `${table_name}`;
+  }
+};
+
 // Collect data for each set_id
 export const getBoardDetails = async ({board_name, layout_id, size_id, set_ids} : ParsedBoardRouteParameters) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -9,35 +18,38 @@ export const getBoardDetails = async ({board_name, layout_id, size_id, set_ids} 
 
   for (const set_id of set_ids) {
     // Get image filename
-    const { rows } = await sql`
+    const { rows } = await sql.query(`
         SELECT image_filename
-        FROM product_sizes_layouts_sets
-        WHERE layout_id = ${layout_id}
-        AND product_size_id = ${size_id}
-        AND set_id = ${set_id}
-      `;
+        FROM ${getTableName(board_name, "product_sizes_layouts_sets")} product_sizes_layouts_sets
+        WHERE layout_id = $1
+        AND product_size_id = $2
+        AND set_id = $3
+      `, [ layout_id, size_id, set_id ]);
 
+      
     if (rows.length === 0) continue;
-
+      
     const imageFilename = rows[0].image_filename;
-
+      
     // Extract image filename
     const image_url = imageFilename.split("/")[1];
-
     // Get holds data
-    const { rows: holds } = await sql`
+    const { rows: holds } = await sql.query(`
         SELECT 
           placements.id AS placement_id, 
           mirrored_placements.id AS mirrored_placement_id, 
           holes.x, holes.y
-        FROM holes
-        INNER JOIN placements ON placements.hole_id = holes.id
-        AND placements.set_id = ${set_id}
-        AND placements.layout_id = ${layout_id}
-        LEFT JOIN placements mirrored_placements ON mirrored_placements.hole_id = holes.mirrored_hole_id
-        AND mirrored_placements.set_id = ${set_id}
-        AND mirrored_placements.layout_id = ${layout_id}
-      `;
+        FROM ${getTableName(board_name, "holes")} holes
+        INNER JOIN ${getTableName(board_name, "placements")} placements ON placements.hole_id = holes.id
+        AND placements.set_id = $1
+        AND placements.layout_id = $2
+        LEFT JOIN ${getTableName(
+          board_name,
+          "placements",
+        )} mirrored_placements ON mirrored_placements.hole_id = holes.mirrored_hole_id
+        AND mirrored_placements.set_id = $1
+        AND mirrored_placements.layout_id = $2
+      `, [set_id, layout_id]);
 
     if (holds.length > 0) {
       imagesToHolds[image_url] = holds.map((hold) => [
@@ -50,11 +62,11 @@ export const getBoardDetails = async ({board_name, layout_id, size_id, set_ids} 
   }
 
   // Get size dimensions
-  const { rows: sizeDimensions } = await sql`
+  const { rows: sizeDimensions } = await sql.query(`
     SELECT edge_left, edge_right, edge_bottom, edge_top
-    FROM product_sizes
-    WHERE id = ${size_id}
-  `;
+    FROM ${getTableName(board_name, "product_sizes")}
+    WHERE id = $1
+  `, [ size_id ]);
 
   if (sizeDimensions.length === 0) {
     throw new Error("Size dimensions not found");
@@ -71,26 +83,31 @@ export const getBoardDetails = async ({board_name, layout_id, size_id, set_ids} 
 
 
 export const getBoulderProblem = async (params: ParsedBoardRouteParametersWithUuid): Promise<BoulderProblem> => {
-  return (await sql.query({
-    text: `
+  return (
+    await sql.query({
+      text: `
         SELECT climbs.uuid, climbs.setter_username, climbs.name, climbs.description,
         climbs.frames, climb_stats.angle, climb_stats.ascensionist_count,
         dg.boulder_name as difficulty,
         ROUND(climb_stats.quality_average::numeric, 2) as quality_average,
         ROUND(climb_stats.difficulty_average::numeric - climb_stats.display_difficulty::numeric, 2) AS difficulty_error,
         climb_stats.benchmark_difficulty
-        FROM climbs
-        LEFT JOIN climb_stats ON climb_stats.climb_uuid = climbs.uuid
-        LEFT JOIN difficulty_grades dg on difficulty = ROUND(climb_stats.display_difficulty::numeric)
-        INNER JOIN product_sizes ON product_sizes.id = $1
+        FROM ${getTableName(params.board_name, "climbs")} climbs
+        LEFT JOIN ${getTableName(params.board_name, "climb_stats")} climb_stats ON climb_stats.climb_uuid = climbs.uuid
+        LEFT JOIN ${getTableName(
+          params.board_name,
+          "difficulty_grades",
+        )} dg on dg.difficulty = ROUND(climb_stats.display_difficulty::numeric)
+        INNER JOIN ${getTableName(params.board_name, "product_sizes")} product_sizes ON product_sizes.id = $1
         WHERE climbs.layout_id = $2
         AND product_sizes.id = $3
         AND climbs.uuid = $4
         AND climb_stats.angle = $5
         limit 1
       `,
-    values: [params.size_id, params.layout_id, params.size_id, params.climb_uuid, params.angle]
-  })).rows[0];
+      values: [params.size_id, params.layout_id, params.size_id, params.climb_uuid, params.angle],
+    })
+  ).rows[0];
 };
 
 export type SearchBoulderProblemResult = {
@@ -121,10 +138,16 @@ export const searchBoulderProblems = async (
             ROUND(climb_stats.quality_average::numeric, 2) as quality_average,
             ROUND(climb_stats.difficulty_average::numeric - climb_stats.display_difficulty::numeric, 2) AS difficulty_error,
             climb_stats.benchmark_difficulty
-          FROM climbs
-          LEFT JOIN climb_stats ON climb_stats.climb_uuid = climbs.uuid
-          LEFT JOIN difficulty_grades dg on difficulty = ROUND(climb_stats.display_difficulty::numeric)
-          INNER JOIN product_sizes ON product_sizes.id = $1
+          FROM ${getTableName(params.board_name, "climbs")} climbs
+          LEFT JOIN ${getTableName(
+            params.board_name,
+            "climb_stats",
+          )} climb_stats ON climb_stats.climb_uuid = climbs.uuid
+          LEFT JOIN ${getTableName(
+            params.board_name,
+            "difficulty_grades",
+          )} dg on dg.difficulty = ROUND(climb_stats.display_difficulty::numeric)
+          INNER JOIN ${getTableName(params.board_name, "product_sizes")} product_sizes ON product_sizes.id = $1
           WHERE climbs.layout_id = $2
           AND climbs.is_listed = 1
           AND climbs.is_draft = 0
