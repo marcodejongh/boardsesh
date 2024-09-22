0650b564af03bb9efb18e3a48ec25901a045a6f1
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-09-22 15:12:40 +0300

    Finished dockerised local db setup

--- db/setup-development-db.sh
+++ db/setup-development-db.sh
@@ -14,13 +14,13 @@ export DB_URL="postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDBNA
 # Optional: Specify the database to connect to for admin commands like creating a new database
 export PGDATABASE="postgres" # Connect to the default `postgres` database
 
-# echo "Creating database if it doesnt exist"
-psql postgres -tAc "SELECT 1 FROM pg_database WHERE datname='verceldb'" | grep -q 1 || psql postgres -c "CREATE DATABASE verceldb"
+echo "Creating database if it doesnt exist"
+psql postgres -tAc "SELECT 1 FROM pg_database WHERE datname='verceldb'" | grep -q 1 && psql postgres -c "DROP DATABASE verceldb"
+psql postgres -c "CREATE DATABASE verceldb"
 
-# # echo "Using boardlib to download database"
-# boardlib database tension /db/tmp/tension.db  
-# boardlib database kilter /db/tmp/kilter.db  
-
+# echo "Using boardlib to download database"
+boardlib database tension /db/tmp/tension.db  
+boardlib database kilter /db/tmp/kilter.db  
 
 export TENSION_DB_FILE="/db/tmp/tension.modified.db"
 export KILTER_DB_FILE="/db/tmp/kilter.modified.db"
@@ -33,15 +33,15 @@ rm -rf $KILTER_DB_FILE
 cp /db/tmp/tension.db $TENSION_DB_FILE
 cp /db/tmp/kilter.db  $KILTER_DB_FILE
 
-
 # PG Loader fails to converst the floats in climb_stats & climb_cache_fields
 # so we convert those to regular floats before running pgloader
-echo "Fixing kilter unsigned float columns so pgloader handles them correctly"
-DB_FILE=$KILTER_DB_FILE /db/fix_unsigned_floats.sh
+echo "Fixing problems in kilter SQLite Database"
+DB_FILE=$KILTER_DB_FILE /db/cleanup_sqlite_db_problems.sh
 
-echo "Fixing tension unsigned float columns so pgloader handles them correctly"
-DB_FILE=$TENSION_DB_FILE /db/fix_unsigned_floats.sh
+echo "Fixing problems in tension SQLite Database"
+DB_FILE=$TENSION_DB_FILE /db/cleanup_sqlite_db_problems.sh
 
 echo "Running pgloader"
 DB_FILE=$TENSION_DB_FILE pgloader /db/tension_db.load 
+
 DB_FILE=$KILTER_DB_FILE pgloader /db/kilter_db.load 
