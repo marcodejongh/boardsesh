4b7ccc7888927fa69f95e8e20178d51497590a3d
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-09-23 07:39:35 +0300

    Change redirect to tension

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import React from "react";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect(`/kilter/8/17/26,27/40/ED76B2C0552B4EB9A9BD5C978C4395E5`);
+  redirect(`/tension/10/6/12,13/40`);
 }
