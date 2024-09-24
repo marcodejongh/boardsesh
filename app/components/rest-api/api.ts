cf6734280ae1bee4d2af01a695a352e6cd038583
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-09-24 18:41:26 +0300

    Update api.ts

--- app/components/rest-api/api.ts
+++ app/components/rest-api/api.ts
@@ -9,7 +9,7 @@ import {
   ParsedBoardRouteParameters,
 } from "@/app/lib/types";
 
-const API_BASE_URL = `${process.env.BASE_URL}/api`;
+const API_BASE_URL = `${process.env.BASE_URL || 'https://www.boardsesh.com'}/api`;
 const headers = new Headers({ "ngrok-skip-browser-warning": "true" });
 
 export const fetchResults = async (
