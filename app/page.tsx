6383baca5befbd7268a9a1d1083237e83b3b9fa4
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-09-29 00:24:22 +0200

    Reditect to kilter HW

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,6 @@ import React from "react";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect(`/tension/10/6/12,13/40/list`);
+  // redirect(`/tension/10/6/12,13/40/list`);
+  redirect(`/kilter/8/17/26,27/40/list`);
 }
