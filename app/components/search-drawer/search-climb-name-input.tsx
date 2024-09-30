81bb1885ab50e68654dd975f94a5927d6625f667
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-09-30 16:51:03 +0200

    Use css & media query to jack up font size on iOS

--- app/components/search-drawer/search-climb-name-input.tsx
+++ app/components/search-drawer/search-climb-name-input.tsx
@@ -13,7 +13,7 @@ const SearchClimbNameInput = () => {
     <>
       <Input
         placeholder="Filter climbs..."
-        style={{ width: "100%", fontSize: '16px' }}
+        style={{ width: "100%" }}
         onChange={(e) => {
           updateFilters({
             name: e.target.value
