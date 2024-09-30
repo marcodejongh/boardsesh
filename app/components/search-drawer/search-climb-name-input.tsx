5e30435cc1447a485f5cb79abf294f8a02fb2aef
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-09-30 10:04:19 +0200

    Implement climb name search for mobile
    Abstracted the UI Search params to a seperate context for easier reuse

---
+++ app/components/search-drawer/search-climb-name-input.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React from "react";
+import { Input } from "antd";
+import { useUISearchParams } from "../board-control/ui-searchparams-provider";
+
+
+const SearchClimbNameInput = () => {
+  const { uiSearchParams, updateFilters } = useUISearchParams();
+
+  // Drawer for mobile view
+  return (
+    <>
+      <Input
+        placeholder="Filter climbs..."
+        style={{ width: "100%" }}
+        onChange={(e) => {
+          updateFilters({
+            name: e.target.value
+          });
+        }}
+        value={uiSearchParams.name}
+      />
+    </>
+  );
+};
+
+export default SearchClimbNameInput;
