12ea111329ff8c22107147367f53008b1b09d8a6
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-12-04 10:39:37 +1100

    Remove unused import

--- app/components/queue-control/reducer.ts
+++ app/components/queue-control/reducer.ts
@@ -1,5 +1,4 @@
 import { useReducer } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import { QueueState, QueueAction } from './types';
 import { SearchRequestPagination } from '@/app/lib/types';
 
