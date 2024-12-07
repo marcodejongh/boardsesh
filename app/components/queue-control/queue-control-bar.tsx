eb4b5fe4fc7573aed7bcd6e3714e969f492c9d84
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-12-07 16:23:31 +1100

    Refactor tick button to a new file

--- app/components/queue-control/queue-control-bar.tsx
+++ app/components/queue-control/queue-control-bar.tsx
@@ -8,6 +8,7 @@ import { usePathname } from 'next/navigation';
 import PreviousClimbButton from './previous-climb-button';
 import { BoardName, BoardDetails, Angle } from '@/app/lib/types';
 import QueueList from './queue-list';
+import TickButton from './tick-button';
 import ClimbThumbnail from '../climb-card/climb-thumbnail';
 import { useBoardProvider } from '../board-provider/board-provider-context';
 
@@ -90,19 +91,7 @@ const QueueControlBar: React.FC<QueueControlBar> = ({ boardDetails, angle }: Que
               ) : null}
               <PreviousClimbButton navigate={isViewPage} />
               <NextClimbButton navigate={isViewPage} />
-              <Badge
-                count={
-                  logbook.length > 0
-                    ? logbook.filter((asc) => asc.climb_uuid === currentClimb?.uuid && Number(asc.angle) === angle)
-                        .length
-                    : 0
-                }
-                overflowCount={100}
-                showZero={false}
-                color="cyan"
-              >
-                <Button id="button-tick" type="default" icon={<CheckOutlined />} />
-              </Badge>
+              <TickButton currentClimb={currentClimb} angle={angle} />
             </Space>
           </Col>
         </Row>
---
+++ app/components/queue-control/tick-button.tsx
@@ -0,0 +1,23 @@
+import { Angle, Climb } from '@/app/lib/types';
+import { useBoardProvider } from '../board-provider/board-provider-context';
+import Button from 'antd/es/button';
+import Badge from 'antd/es/badge';
+import { CheckOutlined } from '@ant-design/icons';
+
+export default ({ currentClimb, angle }: { angle: Angle, currentClimb: Climb | null }) => {
+  const { logbook } = useBoardProvider();
+  return (
+    <Badge
+      count={
+        logbook.length > 0
+          ? logbook.filter((asc) => asc.climb_uuid === currentClimb?.uuid && Number(asc.angle) === angle).length
+          : 0
+      }
+      overflowCount={100}
+      showZero={false}
+      color="cyan"
+    >
+      <Button id="button-tick" type="default" icon={<CheckOutlined />} />
+    </Badge>
+  );
+};
