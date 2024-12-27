0b436f43d2673727fc1ffd22a55c752ccb9e877e
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2024-12-27 21:22:30 +1100

    More debug changes that need undoing

--- app/lib/data/queries.ts
+++ app/lib/data/queries.ts
@@ -248,58 +248,6 @@ export const searchClimbs = async (
       `,
     values: queryParameters,
   });
-  console.log(`
-        WITH latest_stats AS (
-          SELECT DISTINCT ON (climb_uuid) *
-          FROM ${getTableName(params.board_name, 'climb_stats')}
-          WHERE angle = $11
-          ORDER BY climb_uuid, id DESC
-        ),
-        filtered_climbs AS (
-          SELECT
-            climbs.uuid, climbs.setter_username, climbs.name, climbs.description,
-            climbs.frames, latest_stats.angle, latest_stats.ascensionist_count,
-            dg.boulder_name as difficulty,
-            ROUND(latest_stats.quality_average::numeric, 2) as quality_average,
-            ROUND(latest_stats.difficulty_average::numeric - latest_stats.display_difficulty::numeric, 2) AS difficulty_error,
-            latest_stats.benchmark_difficulty
-          FROM ${getTableName(params.board_name, 'climbs')} climbs
-          LEFT JOIN latest_stats ON latest_stats.climb_uuid = climbs.uuid
-          LEFT JOIN ${getTableName(
-            params.board_name,
-            'difficulty_grades',
-          )} dg on dg.difficulty = ROUND(latest_stats.display_difficulty::numeric)
-          INNER JOIN ${getTableName(params.board_name, 'product_sizes')} product_sizes ON product_sizes.id = $1
-          WHERE climbs.layout_id = $2
-          AND climbs.is_listed = true
-          AND climbs.is_draft = false
-
-          -- Ensures only boulder problems are found and not routes
-          AND climbs.frames_count = 1
-          AND product_sizes.id = $3
-          AND latest_stats.angle = $11
-          AND latest_stats.ascensionist_count >= $4
-
-          AND climbs.edge_left > product_sizes.edge_left
-          AND climbs.edge_right < product_sizes.edge_right
-          AND climbs.edge_bottom > product_sizes.edge_bottom
-          AND climbs.edge_top < product_sizes.edge_top
-          ${
-            searchParams.minGrade && searchParams.maxGrade
-              ? 'AND ROUND(latest_stats.display_difficulty::numeric, 0) BETWEEN $5 AND $6'
-              : ''
-          }
-          AND latest_stats.quality_average >= $7
-          AND ABS(ROUND(latest_stats.display_difficulty::numeric, 0) - latest_stats.difficulty_average::numeric) <= $8
-          ${climbNameClause}
-        )
-        SELECT *, 
-        (SELECT COUNT(*) FROM filtered_climbs) as total_count
-        FROM filtered_climbs
-        ORDER BY ${safeSortBy} ${searchParams.sortOrder === 'asc' ? 'ASC' : 'DESC'}, filtered_climbs.uuid ASC
-        LIMIT $9 OFFSET $10
-      `);
-      console.log(queryParameters);
   return {
     climbs: query.rows,
     totalCount: query.rows.length > 0 ? query.rows[0].total_count : 0,
