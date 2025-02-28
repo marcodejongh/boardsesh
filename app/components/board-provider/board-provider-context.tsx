7c253a6fb3087cf82f405136d38297b62005cded
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2025-03-01 09:24:43 +1100

    Fix async boardlayout problem

--- app/components/board-provider/board-provider-context.tsx
+++ app/components/board-provider/board-provider-context.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { BoardName, ClimbUuid } from '@/app/lib/types';
 import { IDBPDatabase, openDB } from 'idb';
