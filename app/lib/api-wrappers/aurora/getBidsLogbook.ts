6df5ccc139ab4f456e8e2050f83f78b975a40ffe
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2025-03-01 18:01:03 +1100

    All the typecheck errors

--- app/lib/api-wrappers/aurora/getBidsLogbook.ts
+++ app/lib/api-wrappers/aurora/getBidsLogbook.ts
@@ -3,6 +3,6 @@ import { userSync } from './userSync';
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export async function getBidsLogbook(board: BoardName, token: string, userId: string): Promise<any[]> {
-  const syncResults = await userSync(board, userId, { tables: ['bids'] }, token);
+  const syncResults = await userSync(board, Number(userId), { tables: ['bids'] }, token);
   return syncResults.PUT.bids || [];
 }
