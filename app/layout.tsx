ef3dc6b14fbbebf28eaa3164a5f669389ebbb179
Marco de Jongh <mdejongh@atlassian.com>
2025-07-08 22:41:14 +1000

    More yank fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ import AuroraWarningModal from './components/aurora-warning-modal';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body style={{ margin: 0 }}>
         <Analytics />
         <AntdRegistry>
