0517ca7fc0e93ae81b0c2a250ffd212691219b6f
Marco de Jongh <mdejongh@atlassian.com>
2025-07-11 16:48:25 +1000

    Comment out local dev stuff

--- app/lib/db/db.ts
+++ app/lib/db/db.ts
@@ -5,18 +5,18 @@ import ws from 'ws';
 
 let connectionString = process.env.DATABASE_URL;
 
-// Configuring Neon for local development
-if (process.env.VERCEL_ENV === 'development' || process.env.NODE_ENV === 'development') {
-  connectionString = 'postgres://postgres:password@localhost:5432/main';
-  console.log(connectionString);
-  neonConfig.fetchEndpoint = (host) => {
-    const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
-    return `${protocol}://${host}:${port}/sql`;
-  };
-  const connectionStringUrl = new URL(connectionString);
-  neonConfig.useSecureWebSocket = connectionStringUrl.hostname !== 'localhost';
-  neonConfig.wsProxy = (host) => (host === 'localhost' ? `${host}:4444/v2` : `${host}/v2`);
-}
+// // Configuring Neon for local development
+// if (process.env.VERCEL_ENV === 'development' || process.env.NODE_ENV === 'development') {
+//   connectionString = 'postgres://postgres:password@localhost:5432/main';
+//   console.log(connectionString);
+//   neonConfig.fetchEndpoint = (host) => {
+//     const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
+//     return `${protocol}://${host}:${port}/sql`;
+//   };
+//   const connectionStringUrl = new URL(connectionString);
+//   neonConfig.useSecureWebSocket = connectionStringUrl.hostname !== 'localhost';
+//   neonConfig.wsProxy = (host) => (host === 'localhost' ? `${host}:4444/v2` : `${host}/v2`);
+// }
 
 // Only configure WebSocket constructor in development or when not building
 export const getPool = () => {
